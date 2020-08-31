b0a7d63feebfdd69645a8d7f5b0ae03c0e47d898
unknown <wuzixian@ESG.360ES.CN>
2020-08-31 18:03:00 +0800

    修改随机红包

--- test/src/codeTest/MoneyTest.java
+++ test/src/codeTest/MoneyTest.java
@@ -7,19 +7,18 @@ public class MoneyTest {
       public static ArrayList<Integer> divide(int totalMoney,int totalCount){
           Random rd = new Random();
           ArrayList<Integer> list = new ArrayList<>();
-          int money = totalMoney;
-          int count = totalCount;
-          for(int i= 0; i<totalCount-1; i++){
-              int hongbao = rd.nextInt(money/count*2);
-              money = money -hongbao;
-              count = count-1;
-              list.add(hongbao);
+          int leftMoney = totalMoney;
+          int leftCount = totalCount;
 
+          for(int i = 0; i<totalCount-1;i++){
+              int money = rd.nextInt(leftMoney/leftCount*2);
+              leftMoney = leftMoney - money;
+              list.add(money);
+              leftCount= leftCount-1;
 
           }
-          list.add(money);
+          list.add(leftMoney);
           return list;
-
       }
 
     public static void main(String[] args) {
